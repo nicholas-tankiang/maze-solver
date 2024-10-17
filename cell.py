754244148017bac40c38799209f6eaa6e8a786ce
nicholas-tankiang <nicholas.tankiang210@gmail.com>
2024-10-17 14:16:12 +0800

    add draw_move method for inbetween cells

--- cell.py
+++ cell.py
@@ -31,3 +31,18 @@ class Cell():
             if self.has_bottom_wall:
                 line = Line(Point(x1, y2), Point(x2, y2))
                 self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo = False):
+        color = "red"
+        if undo:
+            color = "gray"
+
+        # unpacking tuple from get_center
+        start_x, start_y = self.get_center()
+        end_x, end_y = to_cell.get_center()
+
+        line = Line(Point(start_x, start_y), Point(end_x, end_y))
+        self._win.draw_line(line, fill_color = color)
+
+    def get_center(self):
+        return (self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2
--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    cell_test = Cell(win)
-    cell_test.draw(70, 70, 50, 50)
-
     c = Cell(win)
     c.has_left_wall = False
     c.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(100, 100, 150, 150)
+
+    c.draw_move(c1)
 
     win.wait_for_close()
 
