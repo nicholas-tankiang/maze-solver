bfc408901b45bf61b98868c204b66cc316308b87
nicholas-tankiang <nicholas.tankiang210@gmail.com>
2024-10-17 13:02:51 +0800

    add cell class that can draw itself

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 from window import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    line_test = Line(Point(50,50), Point(400,400))
-    win.draw_line(line_test, "black")
+    #line_test = Line(Point(50,50), Point(400,400))
+    #win.draw_line(line_test, "black")
+    cell_test = Cell(True, False, False, False, 50, 70, 50, 70, win)
+    cell_test.draw()
     win.wait_for_close()
 
 main()
